5c931a17c85e65f2e24b1d0c2249b6113041a58e
sreesaik <63366238+sreesaik@users.noreply.github.com>
2024-01-30 00:47:11 -0600

    Add files via upload

---
+++ area of circle.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    const double pi = 2.14;
+    double r;
+    cin>> r;
+    double area;
+    area= pi * pow(r,2);
+    cout<<area;
+    
+    
+    
+
+    return 0;
+}
+
