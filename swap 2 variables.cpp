760e98d838b10d342eef5fc51d2b17167ac84eb8
sreesaik <63366238+sreesaik@users.noreply.github.com>
2024-01-29 23:52:33 -0600

    Add files via upload

---
+++ swap 2 variables.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=6;
+    int b=11;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<"\n";
+    cout<<b;
+    
+
+    return 0;
+}
