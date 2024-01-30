21ced07a0577bee490d965a73e88120c8975294d
sreesaik <63366238+sreesaik@users.noreply.github.com>
2024-01-29 23:50:01 -0600

    Add files via upload

---
+++ swap without third variable.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=6;
+    int b=11;
+    a= a+b; //17
+    b= a-b; // 6
+    a= a-b;//11
+    
+    cout<< a;
+    cout << b;
+    
+
+    return 0;
+}
